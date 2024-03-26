c0ca3ce7b2d2652b634942d2ef22da159aed0a43
LucasPapoti <156001201+LucasPapoti@users.noreply.github.com>
2024-03-26 15:53:26 -0300

    Update open lab 26/03

---
+++ CL_game.py
@@ -0,0 +1,86 @@
+import pygame
+import sys
+import CL_utils as utils
+import CL_colors as clr
+import CL_mqtt as mqtt
+
+def showMoves(sq1, x1, y1, f1, sq2, x2, y2, f2, sq3, x3, y3, f3, screen, color=clr.black):
+    utils.write_midleft(x1, y1, f1, screen, sq1, color)
+    utils.write_midleft(x2, y2, f2, screen, sq2, color)
+    utils.write_midleft(x3, y3, f3, screen, sq3, color)
+
+def showTimer(x_0, y_0, fontSize, screen, time=30, color=clr.black):
+    return utils.write_topleft(x_0, y_0, fontSize, screen, "{:.1f}".format(time), color)
+
+def CL_game(x_0, y_0, x, y, tx_0, ty_0, tFontSize, px_0, py_0, pFontSize, screen, fsq1, fsq2, fsq3, sq1x, sq1y, sq2x, sq2y, sq3x, sq3y,
+            c1=clr.white, c2=clr.black, cDf=clr.black, highlight=None, hlColor=clr.red, time=30.0, sq1="", sq2="", sq3="",
+            bgColor=clr.background2, points = 0, decrease = 1):
+    utils.drawBoard(x_0, y_0, x, y, screen, c1, c2, highlight, hlColor)
+    lines = ['8', '7', '6', '5', '4', '3', '2', '1']
+    collumns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    running = True
+    timeRemaining = time
+    timerRect = showTimer(tx_0, ty_0, tFontSize, screen, timeRemaining, cDf)
+    clock = pygame.time.Clock()
+    while running:
+        screen.fill(bgColor, timerRect)
+        timerRect = showTimer(tx_0, ty_0, tFontSize, screen, timeRemaining, cDf)
+        showMoves(sq1, sq1x, sq1y, fsq1, sq2, sq2x, sq2y, fsq2, sq3, sq3x, sq3y, fsq3, screen, cDf)
+        utils.write_topleft(px_0, py_0, pFontSize, screen, str(points), cDf)
+        # Desenhar o tabuleiro por último
+        utils.drawBoard(x_0, y_0, x, y, screen, c1, c2, highlight, hlColor)
+        timeRemaining -= 1 / 60
+        if timeRemaining<=0: #Fim de jogo
+            mqtt.msgOut('END')
+            return(points)
+        clock.tick(60)
+        # Eventos
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    mx, my = pygame.mouse.get_pos()
+                    if x_0 <= mx <= x_0 + x and y_0 <= my <= x_0 + y:  # Se o click foi no tabuleiro
+                        name = collumns[int((mx - x_0) // (x / 8))] + lines[int((my - y_0) // (y / 8))]
+                        mqtt.sqrOut(name)
+                        acertou = mqtt.msgIn() #Espera um bool acertou
+                        if acertou:
+                            sqNew = mqtt.sqrIn() #Espera 2 numeros de 1 a 8 representando o quadrado no final da memoria
+                            return CL_game(x_0, y_0, x, y, tx_0, ty_0, tFontSize, px_0, py_0, pFontSize, screen,
+                                            fsq1, fsq2, fsq3, sq1x, sq1y, sq2x, sq2y, sq3x, sq3y, c1, c2, cDf,
+                                            highlight, hlColor, timeRemaining, sq2, sq3, sqNew, bgColor, points+1)
+                        elif not acertou:
+                            return CL_game(x_0, y_0, x, y, tx_0, ty_0, tFontSize, px_0, py_0, pFontSize, screen,
+                                            fsq1, fsq2, fsq3, sq1x, sq1y, sq2x, sq2y, sq3x, sq3y, c1, c2, cDf,
+                                            highlight, hlColor, timeRemaining - decrease, sq2, sq3, sqNew, bgColor, points)
+
+                        
+
+
+def test():
+    pygame.init()
+
+    # Criando a tela
+    screenWidth, screenHeight = 1000, 800
+    screen = pygame.display.set_mode((screenWidth, screenHeight))
+    pygame.display.set_caption("ChessLab")
+
+    clock = pygame.time.Clock()
+
+    # Tamanho do tabuleiro
+    x, y = 720, 720
+    # Posição inicial para deixar o tabuleiro centralizado
+    x_0 = 40
+    y_0 = (screenHeight - y) // 2
+
+    screen.fill(clr.background2)
+    CL_game(x_0, y_0, x, y, 775, 300, 120, 864, 200, 100, screen, 100, 70, 50, 784, 464, 872, 464, 936, 464, clr.bwhite, 
+            clr.bblack, clr.white, None, clr.green, 30, 'a1', 'b2', 'e4', clr.background2, 0, 1)   
+    # Encerra o pygame
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    test()
---
+++ chesslamqtt.ino
@@ -0,0 +1,252 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi
+const char *ssid = "iPhonePapoti"; // Enter your WiFi name
+const char *password = "31213121";  // Enter WiFi password
+
+// MQTT Broker
+const char *mqtt_broker = "localhost";
+const char *topic = "esp8266/test";
+const char *mqtt_username = "iPhonePapoti";
+const char *mqtt_password = "31213121";
+const int mqtt_port = 1883;
+
+// variaveis
+bool mqttConnected =0;
+int buttonState = 0;
+byte geradaColuna = 1;
+byte geradaFileira = 1;
+bool acertou = 0;
+byte valuecbyte = 0;
+char test[2]; // Character array to hold the test
+
+
+// input pins
+const int geradaColuna0  = 22; //1   
+const int geradaColuna1 =  17; //2
+const int geradaColuna2 =  21; //3
+const int geradaFileira0  = 19; //4
+const int geradaFileira1  = 20; //5    
+const int geradaFileira2  = 14; //6 
+const int subirJogada  = 10; //7
+const int acertoujogada  = 4; //16
+
+// output pins
+const int jogadaColuna0  = 13; //8    
+const int jogadaColuna1 =  11; //9  
+const int jogadaColuna2 =  12; //10
+const int jogadaFileira0  = 9; //11
+const int jogadaFileira1  = 6; //12    
+const int jogadaFileira2  = 7; //13
+const int inicar  = 5;    //14
+const int fim  = 16; //15
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup(void) {
+  // Set software serial baud to 9600;
+  Serial.begin(9600);
+  // connecting to a WiFi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  //connecting to a mqtt broker
+  byte attempt = 0;
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+
+  while (!client.connected() && attempt < 5) {
+      String client_id = "esp8266-client-";
+      client_id += String(WiFi.macAddress());
+      Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+      if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+          Serial.println("Public emqx mqtt broker connected");
+      } else {
+          Serial.print("failed with state ");
+          Serial.print(client.state());
+          delay(2000);
+      }
+      attempt++;
+  }
+  // initialize the pushbutton pin as an input
+  pinMode(geradaColuna0, INPUT);
+  pinMode(geradaColuna1, INPUT);
+  pinMode(geradaColuna2, INPUT);
+  pinMode(geradaFileira0, INPUT);
+  pinMode(geradaFileira1, INPUT);
+  pinMode(geradaFileira2, INPUT);
+  pinMode(subirJogada, INPUT);
+  pinMode(acertoujogada, INPUT);
+
+
+
+  // initialize the LED pin as an output
+  pinMode(jogadaColuna0, OUTPUT);
+  pinMode(jogadaColuna1, OUTPUT);
+  pinMode(jogadaColuna2, OUTPUT);
+  pinMode(jogadaFileira0, OUTPUT);
+  pinMode(jogadaFileira1, OUTPUT);
+  pinMode(jogadaFileira2, OUTPUT);
+  pinMode(inicar, OUTPUT);
+  pinMode(fim, OUTPUT);
+
+
+
+  // publish and subscribe
+  if (attempt < 5) {
+    client.publish(topic, "hello emqx");
+    client.subscribe(topic);
+    mqttConnected = 1;
+  } else {
+    Serial.print("failed to connect ");
+    mqttConnected = 0;
+  }
+  
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.print(topic);
+    Serial.print("Message: ");
+    for(int i = 0; i < length; i++) {
+      Serial.print((char) payload[i]);
+      valuecbyte = payload[i];
+      if (i == 0) {
+        valuecbyte = valuecbyte - 1;
+        if (valuecbyte == 0) {
+          digitalWrite(jogadaFileira0, LOW);
+          digitalWrite(jogadaFileira1, LOW);
+          digitalWrite(jogadaFileira2, LOW);
+        } else if (valuecbyte == 1) {
+          digitalWrite(jogadaFileira0, HIGH);
+          digitalWrite(jogadaFileira1, LOW);
+          digitalWrite(jogadaFileira2, LOW);
+        } else if (valuecbyte == 2) {
+          digitalWrite(jogadaFileira0, LOW);
+          digitalWrite(jogadaFileira1, HIGH);
+          digitalWrite(jogadaFileira2, LOW);
+        } else if (valuecbyte == 3) {
+          digitalWrite(jogadaFileira0, HIGH);
+          digitalWrite(jogadaFileira1, HIGH);
+          digitalWrite(jogadaFileira2, LOW);
+        } else if (valuecbyte == 4) {
+          digitalWrite(jogadaFileira0, LOW);
+          digitalWrite(jogadaFileira1, LOW);
+          digitalWrite(jogadaFileira2, HIGH);
+        } else if (valuecbyte == 5) {
+          digitalWrite(jogadaFileira0, HIGH);
+          digitalWrite(jogadaFileira1, LOW);
+          digitalWrite(jogadaFileira2, HIGH);
+        } else if (valuecbyte == 6) {
+          digitalWrite(jogadaFileira0, LOW);
+          digitalWrite(jogadaFileira1, HIGH);
+          digitalWrite(jogadaFileira2, HIGH);
+        } else if (valuecbyte == 7) {
+          digitalWrite(jogadaFileira0, HIGH);
+          digitalWrite(jogadaFileira1, HIGH);
+          digitalWrite(jogadaFileira2, HIGH);
+        }
+      } else if (i == 1) {
+        valuecbyte = valuecbyte - 1;
+        if (valuecbyte == 0) {
+          digitalWrite(jogadaColuna0, LOW);
+          digitalWrite(jogadaColuna1, LOW);
+          digitalWrite(jogadaColuna2, LOW);
+        } else if (valuecbyte == 1) {
+          digitalWrite(jogadaColuna0, HIGH);
+          digitalWrite(jogadaColuna1, LOW);
+          digitalWrite(jogadaColuna2, LOW);
+        } else if (valuecbyte == 2) {
+          digitalWrite(jogadaColuna0, LOW);
+          digitalWrite(jogadaColuna1, HIGH);
+          digitalWrite(jogadaColuna2, LOW);
+        } else if (valuecbyte == 3) {
+          digitalWrite(jogadaColuna0, HIGH);
+          digitalWrite(jogadaColuna1, HIGH);
+          digitalWrite(jogadaColuna2, LOW);
+        } else if (valuecbyte == 4) {
+          digitalWrite(jogadaColuna0, LOW);
+          digitalWrite(jogadaColuna1, LOW);
+          digitalWrite(jogadaColuna2, HIGH);
+        } else if (valuecbyte == 5) {
+          digitalWrite(jogadaColuna0, HIGH);
+          digitalWrite(jogadaColuna1, LOW);
+          digitalWrite(jogadaColuna2, HIGH);
+        } else if (valuecbyte == 6) {
+          digitalWrite(jogadaColuna0, LOW);
+          digitalWrite(jogadaColuna1, HIGH);
+          digitalWrite(jogadaColuna2, HIGH);
+        } else if (valuecbyte == 7) {
+          digitalWrite(jogadaColuna0, HIGH);
+          digitalWrite(jogadaColuna1, HIGH);
+          digitalWrite(jogadaColuna2, HIGH);
+        }
+      } else if (i == 2) {
+        if (valuecbyte == 1) {
+          digitalWrite(inicar, HIGH);
+          i = length;
+        } else if (valuecbyte == 2) {
+          digitalWrite(fim, HIGH);
+          i = length;
+        }
+      }
+      
+    }
+    Serial.println("---------------------------------");
+}
+
+void loop() {
+  if (mqttConnected) {
+    digitalWrite(inicar, LOW);
+    digitalWrite(fim, LOW);
+    test[1] = '\0'; // Null-terminate the string
+    buttonState = digitalRead(subirJogada);
+    if (buttonState == HIGH) {
+      buttonState = digitalRead(geradaColuna0);
+      if (buttonState == HIGH) {
+        geradaColuna = geradaColuna + 1;
+      }     
+      buttonState = digitalRead(geradaColuna1);
+      if (buttonState == HIGH) {
+        geradaColuna = geradaColuna + 2;
+      }     
+      buttonState = digitalRead(geradaColuna2);
+      if (buttonState == HIGH) {
+        geradaColuna = geradaColuna + 4;
+      }
+      buttonState = digitalRead(geradaFileira0);
+      if (buttonState == HIGH) {
+        geradaFileira = geradaFileira + 1;
+      } 
+      buttonState = digitalRead(geradaFileira1);
+      if (buttonState == HIGH) {
+          geradaFileira = geradaFileira + 2;
+      } 
+      buttonState = digitalRead(geradaFileira2);
+      if (buttonState == HIGH) {
+          geradaFileira = geradaFileira + 4;
+
+      } 
+      test[0] = geradaColuna; // Assign the character to the array
+      client.publish(topic, test);
+      test[0] = geradaFileira; // Assign the character to the array
+      client.publish(topic, test);
+      geradaColuna = 1;
+      geradaFileira = 1;
+      digitalWrite(subirJogada, LOW);
+
+    }
+    buttonState = digitalRead(acertoujogada);
+    if (buttonState == HIGH) {
+      acertou = 0;
+      test[0] = acertou; // Assign the character to the array
+      client.publish(topic, test);
+    }
+    client.loop();
+  }
+}
