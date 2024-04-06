5d43ea29b1fd149c178390933dc05d13e342efb2
migs <86664870+migs39@users.noreply.github.com>
2024-04-05 21:53:13 -0300

    Add files via upload

--- verilogESP/hexa7segJogada.v
+++ verilogESP/hexa7segJogada.v
@@ -20,7 +20,7 @@
  *--------------------------------------------------------------
  */
 
-module hexa7segABC (hexa, display);
+module hexa7segJogada (hexa, display);
     input      [3:0] hexa;
     output reg [6:0] display;
 
@@ -39,14 +39,14 @@ module hexa7segABC (hexa, display);
         
     always @(hexa)
     case (hexa)
-        4'h0:    display = 7'b0001000; // A
-        4'h1:    display = 7'b0000000; // B
-        4'h2:    display = 7'b1000110; // C
-        4'h3:    display = 7'b1000000; // D
-        4'h4:    display = 7'b0000110; // E
-        4'h5:    display = 7'b0001110; // F
-        4'h6:    display = 7'b0000010; // G
-        4'h7:    display = 7'b0001001; // H
+        4'h0:    display = 7'b1111001;
+        4'h1:    display = 7'b0100100;
+        4'h2:    display = 7'b0110000;
+        4'h3:    display = 7'b0011001;
+        4'h4:    display = 7'b0010010;
+        4'h5:    display = 7'b0000010;
+        4'h6:    display = 7'b1111000;
+        4'h7:    display = 7'b0000000;
         default: display = 7'b1111111;
     endcase
 endmodule
